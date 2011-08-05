3e6cf42616621e17649e85771456d273969cc184
Chu Yeoh <chu_tiong@hotmail.com>
2011-08-06 00:24:07 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectChristmas::Application.config.secret_token = '269e28690ad2b3a9b2a2f5bb8a315e16e1adc85b815426a43b796813b0610e89ddad29846d3bb71bfe16fbd6eed4c633639f4d5ec088d038dab7d1e6be26a801'
