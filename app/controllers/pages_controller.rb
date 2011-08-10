6d4fc3a9354d82b3eac7fcb12d37337158716d62
Chu Yeoh <chu_tiong@hotmail.com>
2011-08-10 15:56:51 +1000

    refactor use of title base

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,18 +1,10 @@
 class PagesController < ApplicationController
 
   def about
-    init
     @title = "About"
   end
 
   def contact
-    init
     @title = "Contact"
   end
-
-  private
-  def init
-    @base_title = :"Project Christmas"
-  end  
-
 end
