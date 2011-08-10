78e6533b96806033c98a50029f13418f0fbb4456
Chu Yeoh <chu_tiong@hotmail.com>
2011-08-10 10:15:17 +1000

    add title to contact and about views

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,11 @@
 class PagesController < ApplicationController
+  
   def about
+    @title = "About"
   end
 
   def contact
+    @title = "Contact"
   end
 
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,12 +1,20 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'about'" do
     it "should be successful" do
       get 'about'
       response.should be_success
     end
+    
+    it "should display about on title" do
+      get 'about'
+      response.should have_selector("title",
+                        :content => "Project Christmas | About")
+    end    
+    
   end
 
   describe "GET 'contact'" do
@@ -14,6 +22,11 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+    
+    it "should display about on title" do
+      get 'contact'
+      response.should have_selector("title", :content => "Project Christmas | Contact")
+    end    
   end
 
 end
