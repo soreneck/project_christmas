f7411a580ff1848fbe78892fd1371cf4db2bf479
Chu Yeoh <chu_tiong@hotmail.com>
2011-08-23 23:36:03 +1000

    add some notes to user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,9 @@
 #  updated_at :datetime
 #
 
+# The attr_accessor :name, :email is all done by ActiveRecord by looking at the
+# columns in the Users table in the database.
+
 class User < ActiveRecord::Base
   attr_accessible :name, :email
 end
