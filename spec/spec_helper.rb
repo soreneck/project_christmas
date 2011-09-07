4a3287e762fc0bfe204666f936de96696d7ebff4
Chu Yeoh <chu_tiong@hotmail.com>
2011-09-07 14:27:54 +1000

    refactor test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -25,7 +25,15 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
   
+  # Methods below can be accessed by all spec tests!
   def test_sign_in(user)
     controller.sign_in(user)
   end
+  
+  def integration_sign_in(user)
+    visit signin_path
+    fill_in :email,    :with => user.email
+    fill_in :password, :with => user.password
+    click_button
+  end  
 end
