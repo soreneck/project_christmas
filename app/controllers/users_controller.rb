8bd2dc4bec9c1e1ad4eb8e02ce0260956bb5b933
Chu Yeoh <chu_tiong@hotmail.com>
2011-09-11 13:14:03 +1000

    filter for correct user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   include ApplicationHelper
   
   before_filter :authenticate, :only => [:show, :edit, :update]
+  before_filter :correct_user, :only => [:edit, :update]
   
   def show
     @user = User.find(params[:id])
@@ -26,12 +27,12 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id]) because of before_filter :correct_user
     @title = "Edit user"
   end
 
   def update
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id]) because of before_filter :correct_user
     if @user.update_attributes(params[:user])
       flash[:success] = "Profile updated."
       redirect_to @user
@@ -49,6 +50,11 @@ class UsersController < ApplicationController
   
   def authenticate
     deny_access unless signed_in?
+  end
+  
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
   end
 
 end
