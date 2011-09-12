c161485433224573309d3f31d63cf117a33cc2eb
Chu Yeoh <chu_tiong@hotmail.com>
2011-09-12 14:38:14 +1000

    admin functionality for user

--- lib/tasks/sample_data.rake
+++ lib/tasks/sample_data.rake
@@ -5,7 +5,8 @@ namespace :db do
     User.create!(:name => "Example User",
                  :email => "user@example.com",
                  :password => "foobar",
-                 :password_confirmation => "foobar")
+                 :password_confirmation => "foobar").toggle!(:admin)
+    
     99.times do |n|
       name  = Faker::Name.name
       email = "user-#{n+1}@example.com"
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -144,6 +144,26 @@ describe User do
       end
     end
   end
+
+  describe "admin attribute" do
+    
+    before(:each) do
+      @user = User.create!(@attr)
+    end
+    
+    it "should response to admin" do
+      @user.should respond_to(:admin)
+    end
+    
+    it "should not be an admin by default" do
+      @user.should_not be_admin
+    end
+    
+    it "should be convertible to an admin" do
+      @user.toggle!(:admin)
+      @user.should be_admin
+    end
+  end
 end
 
 def merge_passwords(password)
