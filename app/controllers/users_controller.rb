ced413250ce6f98c7707b78fe5df32b65bb2be7a
Chu Yeoh <chu_tiong@hotmail.com>
2011-09-12 19:38:23 +1000

    update user controller to use try

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -70,7 +70,7 @@ class UsersController < ApplicationController
   end
 
   def admin_user
-    redirect_to(root_path) unless (current_user && current_user.admin?)
+    redirect_to(root_path) unless current_user.try(:admin?)
   end
 
 end
